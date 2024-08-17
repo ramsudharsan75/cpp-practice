30c69a0a2429d7d8bbb6a0bfdb42d1d328a2c22f
RAMSUDHARSAN MANOHARAN <ramsudharsan.manoharan@wheelsup.com>
2024-08-17 22:12:15 +0530

    Added number system

--- FirstCppProject/main.cpp
+++ FirstCppProject/main.cpp
@@ -2,17 +2,9 @@
 #include <string>
 
 int main() {
-    int age;
-    std::string name;
-    std::cout << "Enter your name: ";
-    // name without spaces
-    // std::cin >> name;
-
-    // name with spaces
-    std::getline(std::cin, name);
-
-    std::cout << "Enter your age: ";
-    std::cin >> age;
-
-    std::cout << "Hi " << name << ", you are " << age << " years old." << std::endl;
+    std::string country;
+    std::cout << "Enter your country: ";
+    std::getline(std::cin, country);
+    std::cout << "I've heard great things about " << country << ". I'd like to go sometime.";
+    return 0;
 }
